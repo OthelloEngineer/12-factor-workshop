27c78c7a519cbd51ebe1210c96124414ea195fd5
OthelloEnginner <jolee18@student.sdu.dk>
2025-09-28 20:57:46 +0200

    improved logging default settings

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 mkdir -p ./volumes/loki
-sudo chown 10001:10001 ./volumes/loki
+sudo chown -R 10001:10001 ./volumes/loki
 mkdir -p ./volumes/grafana
-sudo chown 472:472 ./volumes/grafana
+sudo chown -R 472:472 ./volumes/grafana
+sudo chmod -R 777 ./volumes/grafana
+
